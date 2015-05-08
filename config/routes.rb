a4fec2e4aa8e2a11023c89890b21d677dbfc391c
Darrell Cohn <darrellcohn@gmail.com>
2015-05-07 17:25:02 -0700

    Implement picture upload and associations

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'welcome#index'
   resources :users, only: :show
+  resources :pictures, only: :create
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
